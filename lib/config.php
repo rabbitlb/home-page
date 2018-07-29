74d764cbd5daacd9539a252cdb47243d26c079f7
rabbit_lb <rabbit_lb@163.com>
2018-07-29 20:53:06 +0800

    first commit

---
+++ lib/config.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * uuz-h5theme配置文件
+ * Created by PhpStorm.
+ * User: Amamiya Chinatsu's
+ * Date: 2016/3/5
+ * Time: 12:22
+ */
+//定义网站标题
+$title = "Rabbit_lb's Blog";
+
+//定义Chrome theme-color
+$theme_color = "#22849f";
+
+//设置背景图像
+//请将背景文件放入 /images 文件夹
+$background_filename = "gensou.jpg";
+
+//设置头像
+//请将头像文件放入 /images 文件夹
+$avatar_filename = "reimu.jpg";
+
+//设置主标题
+$main_title = "Welcome to My Blog.";
+
+//设置二级标题
+$sec_title = "Hello World !";
+
+//设置连接地址
+//如需设置其他图标，请参考 http://fontawesome.io/icons/
+//设置第一个图标及替代文字
+$icons_01 = "fa-qq";
+$txt_01 = "QQ";
+$url_01 = "http://wpa.qq.com/msgrd?v=3&uin=1731568159&site=qq&menu=yes";
+
+//设置第二个图标及替代文字
+$icons_02 = "fa-github";
+$txt_02 = "GitHub";
+$url_02 = "https://github.com/rabbitlb/";
+
+//设置第三个图标及替代文字
+$icons_03 = "fa-envelope";
+$txt_03 = "E-Mail";
+$url_03 = "mailto:rabbit_lb@163.com";
+
+//设置下方button信息
+$button_name = "Blog";
+$button_url = "http://45.32.73.114/blog";
+
+//设置版权信息
+$copyright = "Amamiya Chinatsu";
+?>
+
