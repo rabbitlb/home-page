31ea2546766632e0adcc68710b14aa541ea6417e
rabbit_lb <rabbit_lb@163.com>
2018-08-05 18:25:57 +0800

    commit

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ include_once 'lib/config.php';
             opacity: 0;
             z-index: 1;
             background-color: #ffffff;
-            background-image: url("https://github.com/rabbitlb/home-page/raw/master/images/gensou.jpg");
+            background-image: url("https://github.com/rabbitlb/home-page/raw/master/images/bg.jpg");
             background-repeat: no-repeat;
             background-size: cover;
             background-position: center center;
@@ -66,43 +66,7 @@ include_once 'lib/config.php';
             <h1><?php echo $main_title; ?></h1>
             <p><?php echo $sec_title; ?></p>
         </header>
-        <!--
-        <hr />
-        <h2>Extra Stuff!</h2>
-        <form method="post" action="#">
-            <div class="field">
-                <input type="text" name="name" id="name" placeholder="Name" />
-            </div>
-            <div class="field">
-                <input type="email" name="email" id="email" placeholder="Email" />
-            </div>
-            <div class="field">
-                <div class="select-wrapper">
-                    <select name="department" id="department">
-                        <option value="">Department</option>
-                        <option value="sales">Sales</option>
-                        <option value="tech">Tech Support</option>
-                        <option value="null">/dev/null</option>
-                    </select>
-                </div>
-            </div>
-            <div class="field">
-                <textarea name="message" id="message" placeholder="Message" rows="4"></textarea>
-            </div>
-            <div class="field">
-                <input type="checkbox" id="human" name="human" /><label for="human">I'm a human</label>
-            </div>
-            <div class="field">
-                <label>But are you a robot?</label>
-                <input type="radio" id="robot_yes" name="robot" /><label for="robot_yes">Yes</label>
-                <input type="radio" id="robot_no" name="robot" /><label for="robot_no">No</label>
-            </div>
-            <ul class="actions">
-                <li><a href="#" class="button">Get Started</a></li>
-            </ul>
-        </form>
-        <hr />
-        -->
+
         <footer>
             <ul class="icons">
                 <li><a href="<?php echo $url_01;?>" class="<?php echo $icons_01;?>"><?php echo $txt_01;?></a></li>
