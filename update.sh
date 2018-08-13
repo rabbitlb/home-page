1cb784a81a20f20aa30a45cba55778d003ed0ebf
rabbit_lb <rabbit_lb@163.com>
2018-08-13 21:26:10 +0800

    commit

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "commit"
 git push
-ssh -v root@45.32.146.89
+ssh root@45.32.146.89
 #curl -H 'API-Key: 5MSL6Q3SVA4EQJYYFEVNJJWUQBMWOHMBRECA' https://api.vultr.com/v1/server/reboot --data 'SUBID=17529207'
 exit
